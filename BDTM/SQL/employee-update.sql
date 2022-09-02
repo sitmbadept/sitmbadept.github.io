1e3fbc3636715ffd64b2c0c3cf7ac0ef17cd7b40
sitmbadept <109233023+sitmbadept@users.noreply.github.com>
2022-09-02 22:44:46 +0530

    Add files via upload

---
+++ BDTM/SQL/employee-update.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+ID text,
+NAME text,
+Age text,
+City text,
+state text,
+country text
+);
+
+
+INSERT INTO employee VALUES ('1', 'John Todd', '35', 'Mumbai', NULL, NULL),
+			    ('2', 'Paul S', 'Twenty Six', 'Tumkuru', NULL, NULL),
+			    ('3', 'Dominic Dom', '40', 'Pune', NULL, NULL),
+			    ('4', 'Rajat', '32', 'Bangalore', NULL, NULL),
+			    ('5', 'Shruti', '28', 'Gandhinagar', NULL, NULL),
+			    ('6', 'Tom', '30', 'Bihar', NULL, NULL);
