754e5eb2baa10e7f7350a543b3a7a1beaadbba5d
sitmbadept <109233023+sitmbadept@users.noreply.github.com>
2022-09-02 23:06:40 +0530

    Add files via upload

--- BDTM/SQL/employee-update.sql
+++ BDTM/SQL/employee-update.sql
@@ -19,4 +19,4 @@ INSERT INTO employee VALUES ('1', 'John Todd', '35', 'Mumbai', NULL, NULL),
 			    ('3', 'Dominic Dom', '40', 'Pune', NULL, NULL),
 			    ('4', 'Rajat', '32', 'Bangalore', NULL, NULL),
 			    ('5', 'Shruti', '28', 'Gandhinagar', NULL, NULL),
-			    ('6', 'Tom', '30', 'Bihar', NULL, NULL);
+			    ('6', 'Tom', '30', 'Patna', NULL, NULL);
